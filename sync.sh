faca5cf440812926fab89027055bb631c1681c6b
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-09 17:35:05 +0300

    2021-06-09 14:35:05

--- sync.sh
+++ sync.sh
@@ -5,8 +5,8 @@ TARGET="${2}"
 USERNAME="${3}"
 TOKEN="${4}"
 
-git clone --mirror "${SOURCE}" /root/git/source && cd /root/git/source
-git remote add target https://"${USERNAME}":"${TOKEN}"@"${TARGET#https://}"
-git push --mirror target
+git clone --mirror "${SOURCE}" '/root/git/source' && cd '/root/git/source'
+git remote add 'target' https://"${USERNAME}":"${TOKEN}"@"${TARGET#https://}"
+git push --mirror 'target'
 
 exit 0
