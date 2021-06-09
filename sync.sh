73195f806990b20a51f19a801640345bf562a289
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-09 17:08:49 +0300

    2021-06-09 14:08:49

--- sync.sh
+++ sync.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-USERNAME="${1}"
-TOKEN="${2}"
-URL="${3}"
+SOURCE="${1}"
+TARGET="${2}"
+USERNAME="${3}"
+TOKEN="${4}"
 
-git remote add sync_storage https://"${USERNAME}":"${TOKEN}"@"${URL#https://}"
-git push -f --all sync_storage
+git clone "${SOURCE}" /root/git/source --origin source && cd /root/git/source
+git remote add target https://"${USERNAME}":"${TOKEN}"@"${TARGET#https://}"
+git push -f --all target
 
 exit 0
