7a69bafa7b7d63c6f8675fb548e00964a11b3f29
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-09 17:31:23 +0300

    2021-06-09 14:31:23

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ TARGET="${2}"
 USERNAME="${3}"
 TOKEN="${4}"
 
-git clone "${SOURCE}" /root/git/source --origin source && cd /root/git/source
+git clone --mirror "${SOURCE}" /root/git/source && cd /root/git/source
 git remote add target https://"${USERNAME}":"${TOKEN}"@"${TARGET#https://}"
 git push --mirror target
 
