2b836e81ebc4f67ff69a13fb9f80a0089e390b67
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-10 16:06:19 +0300

    2021-06-10 13:06:19

--- mirror.sh
+++ mirror.sh
@@ -5,8 +5,15 @@ TARGET="${2}"
 USERNAME="${3}"
 TOKEN="${4}"
 
-git clone --mirror "${SOURCE}" '/root/git/source' && cd '/root/git/source' || exit
-git remote add 'target' https://"${USERNAME}":"${TOKEN}"@"${TARGET#https://}"
-git push -f --mirror 'target'
+git=$( command -v git )
+
+mirror() {
+  ${git} clone --mirror "${SOURCE}" '/root/git/source'  \
+    && cd '/root/git/source' || exit
+  ${git} remote add 'target' https://"${USERNAME}":"${TOKEN}"@"${TARGET#https://}"
+  ${git} push -f --mirror 'target'
+}
+
+mirror
 
 exit 0
