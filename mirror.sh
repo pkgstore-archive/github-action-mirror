ad3c199d0b8b078011f3387f763e8c407fd0215f
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-25 14:31:42 +0300

    2021-06-25 11:31:42

--- mirror.sh
+++ mirror.sh
@@ -7,7 +7,7 @@ TARGET_REPO="${4}"
 TARGET_USER="${5}"
 TARGET_TOKEN="${6}"
 
-git=$( command -v git )
+git="$( command -v git )"
 
 mirror() {
   SOURCE="https://${SOURCE_USER}:${SOURCE_TOKEN}@${SOURCE_REPO#https://}"
