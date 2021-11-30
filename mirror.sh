293c590d0ebbf43b20265c10475ef6c6b779cfa3
Kitsune Solar <kitsune.solar@gmail.com>
2021-11-30 11:12:24 +0300

    2021-11-30 08:12:24

--- mirror.sh
+++ mirror.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
-SOURCE_REPO="${1}"
-SOURCE_USER="${2}"
-SOURCE_TOKEN="${3}"
-TARGET_REPO="${4}"
-TARGET_USER="${5}"
-TARGET_TOKEN="${6}"
+# -------------------------------------------------------------------------------------------------------------------- #
+# INITIALIZATION.
+# -------------------------------------------------------------------------------------------------------------------- #
 
-git="$( command -v git )"
+init() {
+  # Vars.
+  SOURCE_REPO="${1}"
+  SOURCE_USER="${2}"
+  SOURCE_TOKEN="${3}"
+  TARGET_REPO="${4}"
+  TARGET_USER="${5}"
+  TARGET_TOKEN="${6}"
+
+  # Apps.
+  git="$( command -v git )"
+
+  # Run.
+  mirror
+}
+
+init "$@"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# GIT: MIRROR.
+# -------------------------------------------------------------------------------------------------------------------- #
 
 mirror() {
   SOURCE="https://${SOURCE_USER}:${SOURCE_TOKEN}@${SOURCE_REPO#https://}"
   TARGET="https://${TARGET_USER}:${TARGET_TOKEN}@${TARGET_REPO#https://}"
 
   ${git} clone --mirror "${SOURCE}" '/root/git/source' \
-    && cd '/root/git/source' || exit 1
+    && _pushd '/root/git/source' || exit 1
   ${git} remote add 'target' "${TARGET}"
   ${git} push -f --mirror 'target'
+  _popd || exit 1
 }
 
-mirror
+# -------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------< COMMON FUNCTIONS >------------------------------------------------ #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Pushd.
+_pushd() {
+  command pushd "$@" > /dev/null || exit 1
+}
+
+# Popd.
+_popd() {
+  command popd > /dev/null || exit 1
+}
 
 exit 0
